967b2f573b27620690ce5ccb7adcbaebc5432887
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-03-23 15:27:54 +0300

    [FIX]
    The problem with sending to all users.

--- backend/src/sender.py
+++ backend/src/sender.py
@@ -1,6 +1,7 @@
 import aiohttp
 from config import logger, tb_token, admin_ids
 
+
 async def send_to_bot(text: str):
     """
     Send a message to the telegram bot
@@ -10,23 +11,16 @@ async def send_to_bot(text: str):
     try:
         async with aiohttp.ClientSession() as session:
             for user_id in admin_ids:
-                # Send a request to the bot.
                 async with session.get(
                     f"https://api.telegram.org/bot{tb_token}/sendMessage",
                     params={
-                        # You can get it from @username_to_id_bot.
                         "chat_id": user_id,
                         "text": text,
-                        # So that you can customize the text.
                         "parse_mode": "html"
                     }
                 ) as response:
-                    if not response.ok:
-                        logger.error(f'MESSAGE WAS NOT SENT: {text}. {await response.text()}')
-                        return False
-                    else:
-                        logger.error(f'MESSAGE HAS BEEN SENT: {text}.')
-                        return True
+                    pass
+        logger.error(f'MESSAGE HAS BEEN SENT: {text}.')
     except Exception as error:
         logger.error(f"ERROR SEND TO BOT: {error}")
         return False
