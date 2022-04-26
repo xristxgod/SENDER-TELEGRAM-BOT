798ed65dc2d634bd5f79045e92fc29c45a1c0d37
Murad Mamedov <84931791+xristxgod@users.noreply.github.com>
2022-04-26 11:25:03 +0300

    [ADD] p2p config

--- backend/config.py
+++ backend/config.py
@@ -10,6 +10,9 @@ decimals.prec = 10
 tb_token = os.getenv("TB_TOKEN")
 admin_ids = os.getenv("ADMIN_IDS").split(",")
 
+p2p_tb_token = os.getenv("P2P_TB_TOKEN")
+p2p_admin_ids = os.getenv("P2P_ADMIN_IDS").split(",")
+
 symbol = {
     "add": emoji.emojize(":green_circle:"),
     "dec": emoji.emojize(":red_circle:"),
